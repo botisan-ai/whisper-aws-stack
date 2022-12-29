49643c03035528b520d2340e84789e8f22f8d4b1
Simon Liang <no.lhr0909@gmail.com>
2022-12-29 23:07:47 +0800

    got the minimal sample done

--- client.py
+++ client.py
@@ -1,9 +1,56 @@
 import asyncio
 import websockets
+import sounddevice as sd
+import numpy as np
+import torch
 
-async def hello():
+sd.default.samplerate = 16000
+sd.default.device = 'MacBook Pro Microphone'
+sd.default.channels = 1
+sd.default.dtype = 'float32'
+sd.default.blocksize = int(16000 / 1000 * 50)
+
+# async def hello():
+#     async with websockets.connect("ws://localhost:6666/stream/test") as websocket:
+#         await websocket.send(b"Hello world!")
+#         await websocket.close()
+
+# asyncio.run(hello())
+
+model, silero_utils = torch.hub.load('snakers4/silero-vad', 'silero_vad', force_reload=False)
+
+async def get_voice_buffer_from_microphone():
+    loop = asyncio.get_event_loop()
+    queue = asyncio.Queue()
+
+    def callback(data: np.ndarray, frame_count: int, time_info, status):
+        loop.call_soon_threadsafe(queue.put_nowait, data.copy().squeeze())
+
+    stream = sd.InputStream(callback=callback)
+
+    with stream:
+        consecutive_silence = 0
+        buffer = b''
+        while True:
+            data: np.ndarray = await queue.get()
+            confidence = model(torch.from_numpy(data), sd.default.samplerate).item()
+            if confidence > 0.5:
+                consecutive_silence = 0
+                buffer += data.tobytes()
+            else:
+                consecutive_silence += 1
+                if consecutive_silence > 5 and len(buffer) > 0:
+                    yield buffer
+                    consecutive_silence = 0
+                    buffer = b''
+
+async def main():
+    print('Main app ready')
     async with websockets.connect("ws://localhost:6666/stream/test") as websocket:
-        await websocket.send(b"Hello world!")
-        await websocket.close()
+        print('Connected to websocket server')
+        async for data in get_voice_buffer_from_microphone():
+            await websocket.send(data)
+        # await websocket.close()
 
-asyncio.run(hello())
+if __name__ == '__main__':
+    asyncio.run(main())
--- whisper_tasks.py
+++ whisper_tasks.py
@@ -1,6 +1,15 @@
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+import numpy as np
+
 from celery_app import celery_app
 
 @celery_app.task
-def process_audio_stream(audio: bytes, stream_id: str):
-    print(f'Processing audio: {audio} for stream: {stream_id}')
-    return audio
+def process_audio_stream(audio: bytes, stream_id: str) -> str:
+    processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+    model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(language='zh', task='transcribe')
+    inputs = processor(np.frombuffer(audio, dtype=np.float32), sampling_rate=16000, return_tensors="pt")
+    input_features = inputs.input_features
+    generated_ids = model.generate(inputs=input_features)
+    transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return transcription
